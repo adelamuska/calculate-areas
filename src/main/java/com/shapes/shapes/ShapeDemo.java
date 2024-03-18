ea1b1dbafd77a72f3c1dea89e7158a9d761d94dd
adelamuska <amuskollari20@umt.edu.al>
2024-03-18 19:08:25 +0100

    first commit

--- src/main/java/com/shapes/shapes/ShapeDemo.java
+++ src/main/java/com/shapes/shapes/ShapeDemo.java
@@ -8,20 +8,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShapeDemo implements CommandLineRunner {
 
-    private final ShapeService shapeService;
+    private final ShapeService SHAPE_SERVICE;
 
     @Autowired
     public ShapeDemo(ShapeService shapeService) {
-        this.shapeService = shapeService;
+        this.SHAPE_SERVICE = shapeService;
     }
 
 
     @Override
     public void run(String... args) { //throws Exception {
-        System.err.println("Area of square: " + shapeService.calculateArea2D("Square", 3.0));
-        System.err.println("Area of cuboid: " + shapeService.calculateArea3D("Cuboid", 4.0, 3.0, 2.0));
-        System.err.println("Volume of cuboid: " + shapeService.calculateVolume3D("Cuboid", 2.0, 4.0, 5.0));
-        System.err.println("Perimeter of triangle: " + shapeService.calculatePerimeter2D("Triangle", 6.0, 3.4, 3.2));
+        System.err.println("Area of square: " + SHAPE_SERVICE.calculateArea2D("Square", 3.0));
+        System.err.println("Area of cuboid: " + SHAPE_SERVICE.calculateArea3D("Cuboid", 4.0, 3.0, 2.0));
+        System.err.println("Volume of cuboid: " + SHAPE_SERVICE.calculateVolume3D("Cuboid", 2.0, 4.0, 5.0));
+        System.err.println("Perimeter of triangle: " + SHAPE_SERVICE.calculatePerimeter2D("Triangle", 6.0, 3.4, 3.2));
 
         //throw new Exception("Unsupported shape type");
     }
