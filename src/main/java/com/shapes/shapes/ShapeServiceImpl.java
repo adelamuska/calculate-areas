abec3a9736b073c11950186dac00fb19f986bdf5
adelamuska <amuskollari20@umt.edu.al>
2024-03-18 20:04:29 +0100

    first commit

--- src/main/java/com/shapes/shapes/ShapeServiceImpl.java
+++ src/main/java/com/shapes/shapes/ShapeServiceImpl.java
@@ -7,17 +7,17 @@ import java.util.List;
 @Service
 public class ShapeServiceImpl implements ShapeService{
 
-    final private List<Shape2D> SHAPE2D;
-    final private List<Shape3D> SHAPE3D;
+    final private List<Shape2D> shape2d;
+    final private List<Shape3D> shape3D;
 
     public ShapeServiceImpl(List<Shape2D> shape2D, List<Shape3D> shape3D) {
-        this.SHAPE2D = shape2D;
-        this.SHAPE3D = shape3D;
+        this.shape2d = shape2D;
+        this.shape3D = shape3D;
     }
 
     @Override
     public double calculateArea2D(String shapeType, double...parameters) {
-        return SHAPE2D.stream()
+        return shape2d.stream()
                 .filter(shape -> shape.isShapeTypeSupported(shapeType))
                 .findFirst()
                 .map(shape2D -> shape2D.calculateArea(parameters))
@@ -26,21 +26,21 @@ public class ShapeServiceImpl implements ShapeService{
 
     @Override
     public double calculatePerimeter2D(String shapeType, double... parameters) {
-        return SHAPE2D.stream().filter(shape -> shape.isShapeTypeSupported(shapeType))
+        return shape2d.stream().filter(shape -> shape.isShapeTypeSupported(shapeType))
                 .findFirst().map(shape2D -> shape2D.calculatePerimeter(parameters))
                 .orElseThrow(()-> new NullPointerException("List is null"));
     }
 
     @Override
     public double calculateArea3D(String shapeType, double... parameters) {
-        return SHAPE3D.stream().filter(shape-> shape.isShapeTypeSupported(shapeType))
+        return shape3D.stream().filter(shape-> shape.isShapeTypeSupported(shapeType))
                 .findFirst().map(shape3D -> shape3D.calculateArea(parameters))
                 .orElseThrow(()-> new NullPointerException("List is null"));
     }
 
     @Override
     public double calculateVolume3D(String shapeType, double... parameters) {
-        return SHAPE3D.stream().filter(shape-> shape.isShapeTypeSupported(shapeType))
+        return shape3D.stream().filter(shape-> shape.isShapeTypeSupported(shapeType))
                 .findFirst().map(shape3D -> shape3D.calculateVolume(parameters))
                 .orElseThrow(()-> new NullPointerException("List is null"));
     }
